cd3e13cd20e55f2a747c0c27761b7b717ebf6518
afzalul <afzalul@WSHYD-012.WILCO.COM>
2019-06-10 13:26:21 +0530

    Pushing the updated Test Script code .

--- src/main/java/testScripts/HotelBookingTest.java
+++ src/main/java/testScripts/HotelBookingTest.java
@@ -1,53 +1,19 @@
-import com.sun.javafx.PlatformUtil;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.Select;
+package testScripts;
+
+import pages.HotelBookingPage;
+import wrappers.Annotationmethods;
 import org.testng.annotations.Test;
 
-public class HotelBookingTest {
+public class HotelBookingTest extends Annotationmethods {
 
-    WebDriver driver = new ChromeDriver();
+	@Test
+	public void hotelSearchVerification() throws InterruptedException {
 
-    @FindBy(linkText = "Hotels")
-    private WebElement hotelLink;
+		HotelBookingPage hotelPage=new HotelBookingPage(driver);
+		hotelPage.menuClick();
+		hotelPage.enterKeys("Indiranagar, Bangalore");
+		hotelPage.selectDropDown("1 room, 2 adults");
+		hotelPage.searchClick();
 
-    @FindBy(id = "Tags")
-    private WebElement localityTextBox;
-
-    @FindBy(id = "SearchHotelsButton")
-    private WebElement searchButton;
-
-    @FindBy(id = "travellersOnhome")
-    private WebElement travellerSelection;
-
-    @Test
-    public void shouldBeAbleToSearchForHotels() {
-        setDriverPath();
-
-        driver.get("https://www.cleartrip.com/");
-        hotelLink.click();
-
-        localityTextBox.sendKeys("Indiranagar, Bangalore");
-
-        new Select(travellerSelection).selectByVisibleText("1 room, 2 adults");
-        searchButton.click();
-
-        driver.quit();
-
-    }
-
-    private void setDriverPath() {
-        if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
-        }
-        if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
-        if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
-        }
-    }
-
+	}
 }
--- src/main/java/testScripts/SignInTest.java
+++ src/main/java/testScripts/SignInTest.java
@@ -1,51 +1,19 @@
-import com.sun.javafx.PlatformUtil;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
+package testScripts;
+
+import pages.SignInPage;
+import wrappers.Annotationmethods;
 import org.testng.annotations.Test;
 
-public class SignInTest {
+public class SignInTest extends Annotationmethods {
 
-    WebDriver driver = new ChromeDriver();
-
-    @Test
-    public void shouldThrowAnErrorIfSignInDetailsAreMissing() {
-
-        setDriverPath();
-
-        driver.get("https://www.cleartrip.com/");
-        waitFor(2000);
-
-        driver.findElement(By.linkText("Your trips")).click();
-        driver.findElement(By.id("SignIn")).click();
-
-        driver.findElement(By.id("signInButton")).click();
-
-        String errors1 = driver.findElement(By.id("errors1")).getText();
-        Assert.assertTrue(errors1.contains("There were errors in your submission"));
-        driver.quit();
-    }
-
-    private void waitFor(int durationInMilliSeconds) {
-        try {
-            Thread.sleep(durationInMilliSeconds);
-        } catch (InterruptedException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-    }
-
-    private void setDriverPath() {
-        if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
-        }
-        if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
-        if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
-        }
-    }
-
-
+	@Test
+	public void signInErrorMessageVerification() {
+		SignInPage signIn=new SignInPage(driver);
+		signIn.click("Your trips","Link");
+		signIn.click("SignIn","id");
+		signIn.frameSignInPopup();
+		waitFor(2000);
+		signIn.click("signInButton","id");
+		signIn.signInPageError("errors1","id","There were errors in your submission");
+	}
 }
